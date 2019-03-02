04dc12459bc31011f0c2816b71855d7370397aa7
cefiro <cefiro666@mail.ru>
2019-03-02 17:58:50 +0700

    ...

--- Программирование/с++ и с# проекты/game/game/game.cpp
+++ Программирование/с++ и с# проекты/game/game/game.cpp
@@ -1,13 +1,15 @@
 ﻿#include "pch.h"
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 using namespace sf;
 using namespace std;
 
 int main()
 {
+	Clock clock; //создаем переменную времени, т.о. привязка ко времени(а не загруженности/мощности процессора).
 	RenderWindow window(VideoMode(800, 600), "Lesson 1. kychka-pc.ru");
-	CircleShape shape(30);
+	CircleShape shape(10);
 	shape.setFillColor(Color::Green);
 	float x = 100;
 	float y = 100;
@@ -18,6 +20,10 @@ int main()
 		Event event;
 		while (window.pollEvent(event))
 		{
+			float time = clock.getElapsedTime().asMicroseconds(); //дать прошедшее время в микросекундах
+			clock.restart(); //перезагружает время
+			time = time / 800; //скорость игры
+			cout << time << "\n";//смотрим как живет время (перезагружается, как видим)
 			if (event.type == Event::Closed)
 				window.close();
 		}
@@ -34,25 +40,25 @@ int main()
 		
 		if (Keyboard::isKeyPressed(Keyboard::Right)) {
 			if (!(shape.getPosition().x >= window.getSize().x-shape.getRadius()*2 )) {
-				x += 0.1;
+				x += 0.25;
 				shape.setPosition(x, y);
 			}
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Up)) {
 			if (!(shape.getPosition().y <= 0)) {
-				y -= 0.1;
+				y -= 0.25;
 				shape.setPosition(x, y);
 			}
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Left)) {
 			if (!(shape.getPosition().x <= 0)) {
-				x -= 0.1;
+				x -= 0.25;
 				shape.setPosition(x, y);
 			}
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Down)) {
 			if (!(shape.getPosition().y >= window.getSize().y - shape.getRadius() * 2)) {
-				y += 0.1;
+				y += 0.25;
 				shape.setPosition(x, y);
 			}
 		}
