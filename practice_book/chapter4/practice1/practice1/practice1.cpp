4bfdcda6fec5ba6c187b6aa36709d9cbc2166e24
cefiro <cefiro666@mail.ru>
2019-03-19 15:42:38 +0700

    ...

---
+++ practice_book/chapter4/practice1/practice1/practice1.cpp
@@ -0,0 +1,36 @@
+﻿// упражнение 1
+
+#include "pch.h"
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	struct Number
+	{
+		unsigned int city_code;
+		unsigned int station_number;
+		unsigned int subscriber_number;
+	};
+
+	Number my_number = { 212,767,8900 };
+	Number your_number;
+
+    cout << "Введите код города, номер станции и номер абонента:\n";
+
+	cin >> your_number.city_code >> your_number.station_number 
+		>>your_number.subscriber_number;
+
+	cout << "Мой номер (" << my_number.city_code << ") " 
+		<< my_number.station_number << "-" 
+		<< my_number.subscriber_number << endl;
+
+	cout << "Ваш номер (" << your_number.city_code << ") " 
+		<< your_number.station_number << "-" 
+		<< your_number.subscriber_number << endl;
+}
