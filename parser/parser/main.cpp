485acf82839b6202b5ce10490f882f5dbf2e32a4
cefiro <cefiro666@mail.ru>
2019-03-28 23:12:19 +0700

    ///

--- parser/parser/main.cpp
+++ parser/parser/main.cpp
@@ -7,15 +7,107 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <list>
+#include <vector>
 
 using namespace std;
+
+// форматирование ссылок и удаление лишних тегов из абзацев
+void formatLink(string& buff)
+{
+	string openTeg, closeTeg;
+	string a;
+	unsigned int begin = 0;
+	unsigned int end;
+	unsigned int pos = 0;
+	pos = 0;
+	while (true) {
+		openTeg = "<a href=\"";
+		closeTeg = "\"";
+		begin = buff.find(openTeg, pos);
+		if (begin == string::npos) break;
+		end = buff.find(closeTeg, begin + openTeg.size());
+		a = " [" + buff.substr(begin + openTeg.size(), end -
+			(begin + openTeg.size())) + "]";
+
+		closeTeg = ">";
+		end = buff.find(closeTeg, begin + openTeg.size());
+		if (end == string::npos) break;
+		buff.erase(begin, end - begin + closeTeg.size());
+
+		openTeg = "</a>";
+		begin = buff.find(openTeg, pos);
+		buff.erase(begin, openTeg.size());
+		buff.insert(begin, a);
+
+		openTeg = "<a href=\"";
+		pos = begin + a.size();
+	}
+
+	// удаление лишних тегов
+	string arr[2] = { "span", "i" };
+	for (int i = 0; i < 2; i++)
+	{
+		pos = 0;
+		while (true) {
+			openTeg = "<" + arr[i];
+			closeTeg = ">";
+			begin = buff.find(openTeg, pos);
+			if (begin == string::npos) break;
+			end = buff.find(closeTeg, begin + openTeg.size());
+			buff.erase(begin, end - begin + closeTeg.size());
+		}
+		pos = 0;
+		while (true) {
+			openTeg = "</" + arr[i];
+			closeTeg = ">";
+			begin = buff.find(openTeg, pos);
+			if (begin == string::npos) break;
+			end = buff.find(closeTeg, begin + openTeg.size());
+			buff.erase(begin, end - begin + closeTeg.size());
+		}
+	}
+}
+
+
+
+
+// рекурсивный парсер оглавления и абзацев
+void parser(string data, int pos, string& buff, list<int> arrBegin, vector<string> arrTeg)
+{
+	arrBegin.clear();
+	for (unsigned int i = 0; i < arrTeg.size(); i++)
+	{
+		if (data.find(arrTeg[i], pos) != string::npos)
+		{
+			arrBegin.push_back(data.find(arrTeg[i], pos));
+		}
+	}
+	if (!arrBegin.empty())
+	{
+		arrBegin.sort();
+
+		int begin = data.find(">", arrBegin.front());
+		string teg = data.substr(arrBegin.front() + 1, begin -
+			arrBegin.front() - 1);
+		int end = data.find("</" + teg, begin + 1);
+		buff.append(data.substr(begin + 1, end -
+			begin - 1) + "\n\n");
+		
+		parser(data, end, buff, arrBegin, arrTeg);
+	}
+}
+
+
+
 
 int main(int argc, char* argv[])
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	LPCSTR URL = argv[1];
+	//LPCSTR URL = argv[1];
+	LPCSTR URL = "https://lenta.ru/news/2019/03/28/uvajenie/";
 	string url = URL;
 	while (true)
 	{
@@ -65,94 +157,22 @@ int main(int argc, char* argv[])
 		}
 		InternetCloseHandle(hOpenUrlA);
 
-
-
 		ofstream  text(path, ios::out | ios::binary);
 
-		string openTeg, closeTeg;
 		string buff;
-		string a;
-		int begin = 0;
-		int end;		
 		unsigned int pos = 0;
 
-		// парсинг заголовка
-		openTeg = "<title>";
-		closeTeg = "</title>";
-		while (true) {
-			begin = data.find(openTeg, pos);
-			if (begin == string::npos) break;
-			end = data.find(closeTeg, begin + openTeg.size());
-			// записываем в буффер заголовок с отбивкой строки
-			buff.append(data.substr(begin + openTeg.size(), end - 
-				(begin + openTeg.size())) + "\n\n");
-			pos = end + closeTeg.size();
-		}
+		list<int> arrBegin(0);
+		vector<string> arrTeg = { "<title>","<p>" };
 
-		// парсинг абзацев
-		pos = 0;
-		while (true) {
-			openTeg = "<p>";
-			closeTeg = "</p>";
-			begin = data.find(openTeg, pos);
-			if (begin == string::npos) break;
-			end = data.find(closeTeg, begin + openTeg.size());
-			buff.append(data.substr(begin + openTeg.size(), end - 
-				(begin + openTeg.size())) + "\n\n");
-			pos = end + closeTeg.size();
-		}
+		parser(data, pos, buff, arrBegin, arrTeg);
 
-		// форматирование ссылок
-		pos = 0;
-		while (true) {
-			openTeg = "<a href=\"";
-			closeTeg = "\"";
-			begin = buff.find(openTeg, pos);
-			if (begin == string::npos) break;
-			end = buff.find(closeTeg, begin + openTeg.size());
-			a = " [" + buff.substr(begin + openTeg.size(), end -
-				(begin + openTeg.size())) + "]";
-
-			closeTeg = ">";
-			end = buff.find(closeTeg, begin + openTeg.size());
-			if (end == string::npos) break;
-			buff.erase(begin, end - begin + closeTeg.size());
-
-			openTeg = "</a>";
-			begin = buff.find(openTeg, pos);
-			buff.erase(begin, openTeg.size());
-			buff.insert(begin, a);
-
-			openTeg = "<a href=\"";
-			pos = begin + a.size();
-		}
-
-		// удаление лишних тегов
-		string arr[2] = { "span", "i" };
-		for (int i = 0; i < 2; i++)
-		{
-			pos = 0;
-			while (true) {
-				openTeg = "<" + arr[i];
-				closeTeg = ">";
-				begin = buff.find(openTeg, pos);
-				if (begin == string::npos) break;
-				end = buff.find(closeTeg, begin + openTeg.size());
-				buff.erase(begin, end - begin + closeTeg.size());
-			}
-			pos = 0;
-			while (true) {
-				openTeg = "</" + arr[i];
-				closeTeg = ">";
-				begin = buff.find(openTeg, pos);
-				if (begin == string::npos) break;
-				end = buff.find(closeTeg, begin + openTeg.size());
-				buff.erase(begin, end - begin + closeTeg.size());
-			}
-		}
+		//formatLink(buff);
+	
 		text << buff;
 		cout << "Фаил сохранен!" << endl;
 		text.close();
 	} 
 	InternetCloseHandle(hInternet);
 }
+
