75b9d3a68c4ea8e03094e7427be1df5cf9836de6
cefiro666 <45936078+cefiro666@users.noreply.github.com>
2019-03-28 16:29:54 +0700

    ...

--- parser/parser/main.cpp
+++ parser/parser/main.cpp
@@ -7,7 +7,6 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
@@ -16,7 +15,20 @@ int main(int argc, char* argv[])
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	LPCSTR URL = "https://edaplus.info/produce/carrot.html";
+	LPCSTR URL = argv[1];
+	string url = URL;
+	while (true)
+	{
+		if (url.find("/") == string::npos) break;
+		url.replace(url.find("/"), 1, "_");
+	}
+	while (true)
+	{
+		if (url.find("?") == string::npos) break;
+		url.replace(url.find("?"), 1, "_");
+
+	}
+	string path = url.substr(8,(url.size()-8)) + "_index.txt";
 
 	// инициализация WinInet
 	HINTERNET hInternet = InternetOpenA("WinInet", INTERNET_OPEN_TYPE_PRECONFIG, 0, 0, 0);
@@ -26,63 +38,121 @@ int main(int argc, char* argv[])
 	{
 		HINTERNET hOpenUrlA = InternetOpenUrlA(hInternet, URL, 0, 0, 0, 0);
 
-		// считывание файла
+		string data;
+
+		// считывание страницы в строковую переменную
+
 		if (hOpenUrlA != NULL)
 		{
-			// создаём выходной файл
-			ofstream file("news.txt", ios::out | ios::binary);
-			if (file.is_open())
-			{
-				char buffer[1024];
-				BOOL hReadFile;
-				DWORD bytesRead;
+			char buffer[1024];
+			BOOL hReadFile;
+			DWORD bytesRead;
 
-				do {
-					// читаем данные
-					hReadFile = InternetReadFile(hOpenUrlA, buffer, sizeof(buffer) - 1, &bytesRead);
+			do {
+				// читаем данные в буфер
+				hReadFile = InternetReadFile(hOpenUrlA, buffer, sizeof(buffer) - 1, &bytesRead);
+				buffer[bytesRead] = 0;
 
-					// сохраняем результат
-					buffer[bytesRead] = 0;
-					file << buffer;
-
-				} while (!(hReadFile == FALSE || bytesRead == 0));
-				file.close();
-				ofstream  file111("news111.txt", ios::out | ios::binary);
-				string date;
-				ifstream file("news.txt", ios::in);
-				while (!(date == "</html>"))
+				// записываем данные из буфера в строку
+				for (unsigned int i = 0; i < bytesRead; i++)
 				{
-					getline(file, date, '<');
-					//file.seekg(-1, ios_base::cur);
-					getline(file, date, '>');
-					date = "<" + date + ">";
-					if (date == "<p>")
-					{
-						getline(file, date, '<');
-						file111 << date << "\n\n";
-						file.seekg(3, ios_base::cur);
-					}
-					else if (date == "<h1>" || date == "<h2>" || date == "<h3>" || date == "<h4>" || date == "<h5>" || date == "<h6>")
-					{
-						getline(file, date, '<');
-						file111 << date << "\n\n";
-						file.seekg(4, ios_base::cur);
-					}
-					else if (date == "<title>")
-					{
-						getline(file, date, '<');
-						file111 << date << "\n\n";
-						file.seekg(7, ios_base::cur);
-					}
-
+					data.push_back(buffer[i]);
 				}
-				file111.close();
-				file.close();
-			} else {
-				cout << "Ошибка записи файла!" << endl;
-			}
+			} while (!(hReadFile == FALSE || bytesRead == 0));
+		}
+		else {
+			cout << "Нет соединения с интернетом!" << endl;
 		}
 		InternetCloseHandle(hOpenUrlA);
-	}
+
+
+
+		ofstream  text(path, ios::out | ios::binary);
+
+		string openTeg, closeTeg;
+		string buff;
+		string a;
+		int begin = 0;
+		int end;		
+		unsigned int pos = 0;
+
+		// парсинг заголовка
+		openTeg = "<title>";
+		closeTeg = "</title>";
+		while (true) {
+			begin = data.find(openTeg, pos);
+			if (begin == string::npos) break;
+			end = data.find(closeTeg, begin + openTeg.size());
+			// записываем в буффер заголовок с отбивкой строки
+			buff.append(data.substr(begin + openTeg.size(), end - 
+				(begin + openTeg.size())) + "\n\n");
+			pos = end + closeTeg.size();
+		}
+
+		// парсинг абзацев
+		pos = 0;
+		while (true) {
+			openTeg = "<p>";
+			closeTeg = "</p>";
+			begin = data.find(openTeg, pos);
+			if (begin == string::npos) break;
+			end = data.find(closeTeg, begin + openTeg.size());
+			buff.append(data.substr(begin + openTeg.size(), end - 
+				(begin + openTeg.size())) + "\n\n");
+			pos = end + closeTeg.size();
+		}
+
+		// форматирование ссылок
+		pos = 0;
+		while (true) {
+			openTeg = "<a href=\"";
+			closeTeg = "\"";
+			begin = buff.find(openTeg, pos);
+			if (begin == string::npos) break;
+			end = buff.find(closeTeg, begin + openTeg.size());
+			a = " [" + buff.substr(begin + openTeg.size(), end -
+				(begin + openTeg.size())) + "]";
+
+			closeTeg = ">";
+			end = buff.find(closeTeg, begin + openTeg.size());
+			if (end == string::npos) break;
+			buff.erase(begin, end - begin + closeTeg.size());
+
+			openTeg = "</a>";
+			begin = buff.find(openTeg, pos);
+			buff.erase(begin, openTeg.size());
+			buff.insert(begin, a);
+
+			openTeg = "<a href=\"";
+			pos = begin + a.size();
+		}
+
+		// удаление лишних тегов
+		string arr[2] = { "span", "i" };
+		for (int i = 0; i < 2; i++)
+		{
+			pos = 0;
+			while (true) {
+				openTeg = "<" + arr[i];
+				closeTeg = ">";
+				begin = buff.find(openTeg, pos);
+				if (begin == string::npos) break;
+				end = buff.find(closeTeg, begin + openTeg.size());
+				buff.erase(begin, end - begin + closeTeg.size());
+			}
+			pos = 0;
+			while (true) {
+				openTeg = "</" + arr[i];
+				closeTeg = ">";
+				begin = buff.find(openTeg, pos);
+				if (begin == string::npos) break;
+				end = buff.find(closeTeg, begin + openTeg.size());
+				buff.erase(begin, end - begin + closeTeg.size());
+			}
+		}
+		text << buff;
+		cout << "Фаил сохранен!" << endl;
+		text.close();
+	} 
 	InternetCloseHandle(hInternet);
 }
