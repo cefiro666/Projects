9c7f9ac69c8c102fe34af26ac46053e3aa158d44
cefiro666 <45936078+cefiro666@users.noreply.github.com>
2019-04-30 15:00:39 +0700

    ...

---
+++ practiceBook/chapter14/practice1/practice1/practice1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+template <class T> 
+T summ(T* arr, int size)
+{
+	T summ = 0;
+	for (int i = 0; i < size; i++)
+	{
+		summ += arr[i];
+	}
+
+	return summ / size;
+}
+
+int main()
+{
+	int arr1[] = { 5, 67, 9 };
+	float arr2[] = { 5.6F, 4.56F, 9.3F };
+	std::cout << summ(arr1, 3) << std::endl;
+	std::cout << summ(arr2, 3);
+}
---
+++ practiceBook/chapter14/practice2/practice2/practice2.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+const int MAX = 10;
+
+template <class T>
+class Turn
+{
+public:
+	Turn() : m_tail(-1), m_head(-1) {}
+
+	void push(T value)
+	{
+		arr[++m_tail] = value;
+		if (m_tail >= MAX - 1)
+		{
+			m_tail--;
+		}
+	}
+
+	T pop()
+	{
+		T temp = arr[++m_head];
+
+		if (m_head >= MAX - 1)
+		{
+			m_head--;
+		}
+
+		return temp;
+	}
+
+private:
+	T arr[MAX];
+
+	int m_head;
+	int m_tail;
+};
+
+int main()
+{
+	Turn<int> turn1;
+	Turn<float> turn2;
+
+	turn1.push(5);
+	turn1.push(8);
+	turn1.push(34);
+	turn1.push(2);
+	turn1.push(5);
+	turn1.push(8);
+	turn1.push(34);
+	turn1.push(2);
+	turn1.push(5);
+	turn1.push(8);
+	turn1.push(34);
+	turn1.push(2);
+	std::cout << turn1.pop() << std::endl;
+	std::cout << turn1.pop() << std::endl;
+	std::cout << turn1.pop() << std::endl;
+	std::cout << turn1.pop() << std::endl;
+	turn1.push(89);
+	turn1.push(789);
+	std::cout << turn1.pop() << std::endl;
+	std::cout << turn1.pop() << std::endl;
+
+	turn2.push(5.45F);
+	turn2.push(8.32F);
+	turn2.push(3.4F);
+	turn2.push(2.90F);
+	std::cout << turn2.pop() << std::endl;
+	std::cout << turn2.pop() << std::endl;
+	std::cout << turn2.pop() << std::endl;
+	std::cout << turn2.pop() << std::endl;
+	turn2.push(8.9F);
+	turn2.push(78.9F);
+	std::cout << turn2.pop() << std::endl;
+	std::cout << turn2.pop() << std::endl;
+}
