7f9214a655a394c0e946a8d6e0ca201de1a99910
cefiro <cefiro666@mail.ru>
2019-05-21 23:55:55 +0700

    ...

---
+++ страуструп/chapter6/practice18/practice18/practice18.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <map>
+
+std::map<std::string, double> table;
+
+enum TokenValue
+{
+	NAME,			NUMBER,			END,
+	PLUS = '+',		MINUS = '-',	MUL = '*',	DIV = '/',
+	PRINT = ';',	ASSIGN = '=',	LP = '(',	RP = ')'
+};
+
+TokenValue currTok = PRINT;
+
+double numberValue;
+std::string stringValue;
+
+TokenValue getToken()
+{
+	char ch = 0;
+	std::cin >> ch;
+	switch (ch)
+	{
+	case 0:
+		return currTok = END;
+	}
+}
+
+double expr(bool get);
+
+double prim(bool get)
+{
+	if (get) getToken();
+
+	switch (currTok)
+	{
+	case NUMBER:
+		double v = numberValue;
+		getToken();
+		return v;
+	case NAME:
+		double& v = table[stringValue];
+		if (getToken() == ASSIGN) v = expr(true);
+		return v;
+	case MINUS:
+		return -prim(true);
+	case LP:
+		double e = expr(true);
+		if (currTok != RP) return error("')' expected");
+		getToken();
+		return e;
+	default:
+		return error("primary ecpected");
+	}
+}
+
+double term(bool get)
+{
+	double left = prim(get);
+	
+	while (true)
+	{
+		switch (currTok)
+		{
+		case MUL:
+			left *= prim(true);
+			break;
+		case DIV:
+			if (double d = prim(true))
+			{
+				left /= d;
+				break;
+			}
+			return error("divide by 0");
+		default:
+			return left;
+		}
+	}
+}
+
+double expr(bool get)
+{
+	double left = term(get);
+
+	while(true)
+	{
+		switch (currTok)
+		{
+		case PLUS:
+			left += term(true);
+			break;
+		case MINUS:
+			left -= term(true);
+			break;
+		default:
+			return left;
+		}
+	}
+}
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
---
+++ страуструп/chapter6/practice3/practice3/practice3.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <map>
+#include <algorithm>
+#include <iterator>
+
+namespace Task
+{
+	class Map
+	{
+	public:
+		Map() {}
+		void input()
+		{
+			std::string word;
+			double value;
+			char ch;
+
+			while (true)
+			{
+				std::cout << "Input word: ";
+				std::cin >> word;
+				std::cout << "Input value for \"" << word << "\": ";
+				std::cin >> value;
+
+				i = map.find(word);
+
+				if (i == map.end())
+				{
+					map[word] = { value, 1 };
+
+				} else {
+
+					map[word] = { map[word].value + value, ++map[word].count };
+				}
+
+				std::cout << "Repeat? (y/n)... ";
+				std::cin >> ch;
+				if (ch == 'n') break;
+			}
+		}
+
+		void sumWord()
+		{
+			std::cout << "\nThe sum of the values by words:\n\n";
+			
+			double sumWord;
+			double averageWord;
+			double sum = 0;
+			int count = 0;
+
+			for (i = map.begin(); i != map.end(); i++)
+			{
+				sumWord = i->second.value;
+				averageWord = sumWord / i->second.count;
+
+				std::cout << "Word - \"" << i->first << "\", quantity - " <<
+					i->second.count << ", sum - " << sumWord << ", average - " <<
+					averageWord << '\n';
+
+				sum += sumWord;
+				count += i->second.count;
+			}
+
+			std::cout << "Sum all words - " << sum << ", average all words - " <<
+				sum / count << '\n';
+		}
+
+	private:
+		struct Data
+		{
+			double value;
+			int count;
+		};
+
+		std::map<std::string, Data> map;
+		std::map<std::string, Data>::iterator i;
+	};
+}
+
+
+int main()
+{
+	Task::Map map;
+	
+	map.input();
+	map.sumWord();
+}
